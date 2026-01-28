3aa64b25bcde03d43880693ab1a29d4f202fa686
DXCSithlordPadawan <bofh2bikerider@gmail.com>
2026-01-28 09:33:39 +0000

    Delete bmad_forge directory

--- bmad_forge/bmad_forge/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for BMAD Forge project.
-"""
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmad_forge.settings')
-application = get_wsgi_application()
--- bmad_forge/forge/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services module initialization.
-"""
-
-from .github_sync import GitHubSyncService
-from .template_parser import TemplateParser
-from .bmad_validator import BMADValidator
-
-__all__ = ['GitHubSyncService', 'TemplateParser', 'BMADValidator']
